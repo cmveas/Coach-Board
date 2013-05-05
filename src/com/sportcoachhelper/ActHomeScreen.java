68bd4cb8a2934d1110ddd7744665517bc7edb11b
cmveas <cmveas@yahoo.com.ar>
2013-05-05 15:05:02 -0300

    [17] Scroll Arrows to scroll different play fields

--- src/com/sportcoachhelper/ActHomeScreen.java
+++ src/com/sportcoachhelper/ActHomeScreen.java
@@ -6,6 +6,9 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.sportcoachhelper.fragments.ScreenSlidePageFragment;
 
@@ -13,6 +16,8 @@ public class ActHomeScreen extends FragmentActivity {
 
 	private ViewPager mPager;
 	private ScreenSlidePagerAdapter mPagerAdapter;
+	private ImageView imvLeftArrow;
+	private ImageView imvRightArrow;
 
 	@Override
 	protected void onCreate(Bundle arg0) {
@@ -21,8 +26,62 @@ public class ActHomeScreen extends FragmentActivity {
 
         // Instantiate a ViewPager and a PagerAdapter.
         mPager = (ViewPager) findViewById(R.id.pager);
+        imvLeftArrow = (ImageView) findViewById(R.id.imvLeftArrow);
+        imvRightArrow = (ImageView) findViewById(R.id.imvRightArrow);
+        
+        
         mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
+    
+        
+        
+	    mPager.setOnPageChangeListener(new OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int pos) {
+
+				
+			}
+			
+			@Override
+			public void onPageScrolled(int pos, float arg1, int arg2) {
+				setPositionInAdapter(pos);
+				
+			}
+
+			
+			
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		int current = mPager.getCurrentItem();
+		
+		setPositionInAdapter(current);
+	}
+	
+	
+	private void setPositionInAdapter(int pos) {
+		if(pos==0)  {
+			imvLeftArrow.setVisibility(View.INVISIBLE);
+		} else {
+			imvLeftArrow.setVisibility(View.VISIBLE);
+		}
+		
+		
+		if(pos==mPagerAdapter.getCount()-1)  {
+			imvRightArrow.setVisibility(View.INVISIBLE);
+		} else {
+			imvRightArrow.setVisibility(View.VISIBLE);
+		}
 	}
 	
 	
