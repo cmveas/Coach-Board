ba665b68ac61d094b32e4370bbc39b5dc81c1b84
cmveas <cmveas@yahoo.com.ar>
2013-05-16 21:52:14 -0300

    [Refactor] Create Play object

---
+++ src/com/sportcoachhelper/model/Play.java
@@ -0,0 +1,83 @@
+package com.sportcoachhelper.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import com.sportcoachhelper.paths.ColorPath;
+import com.sportcoachhelper.paths.interfaces.Detectable;
+import com.sportcoachhelper.paths.interfaces.Dibujables;
+
+public class Play implements Serializable {
+	
+	private long lastSaved;
+	private String name;
+	private String field;
+	private ArrayList<Dibujables> undoablePaths = new ArrayList<Dibujables>();
+	
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+	
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<Dibujables> getUndoablePaths() {
+		return undoablePaths;
+	}
+
+	public void setUndoablePaths(ArrayList<Dibujables> undoablePaths) {
+		this.undoablePaths = undoablePaths;
+	}
+
+
+	public void addPath(ColorPath mPath) {
+		undoablePaths.add(mPath);		
+	}
+
+	public boolean removeLast() {
+		boolean result = false;
+		if (undoablePaths.size() > 0) {
+			Dibujables removedPath = undoablePaths.remove(undoablePaths.size() - 1);
+			removedPath.resetPath();		
+			result=true;
+		}
+		return result;
+	}
+
+	public void removeAllMoves() {
+		undoablePaths.removeAll(undoablePaths);
+		
+	}
+
+	public boolean erasedSelected(Detectable movable) {
+		boolean result = false;
+		if(movable!=null && undoablePaths.contains(movable)) {
+			undoablePaths.remove(movable);
+			
+		}
+		return result;
+	}
+
+	public long getLastSaved() {
+		return lastSaved;
+	}
+
+	public void setLastSaved(long lastSaved) {
+		this.lastSaved = lastSaved;
+	}
+
+
+	
+	
+}
