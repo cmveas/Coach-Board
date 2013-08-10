98d77261d3173581c0e577aedab4d5768f6fcd7f
unknown <cmveas@yahoo.com.ar>
2013-08-10 11:16:24 -0300

    Trash can at the end of the tool bar

---
+++ src/com/sportcoachhelper/ActHelpPage.java
@@ -0,0 +1,49 @@
+package com.sportcoachhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+/**
+ * Created by Claudio on 8/1/13.
+ */
+public class ActHelpPage extends Activity {
+
+    private TextView modesHelp;
+    private TextView modeSelection;
+    private TextView modeContinuous;
+    private TextView modeDotted;
+    private TextView roundPlayerHelp;
+    private TextView trianglePlayerHelp;
+    private TextView squarePlayerHelp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.help);
+
+        modesHelp = (TextView) findViewById(R.id.modesHelp);
+        modesHelp.setText(getText(R.string.help_mode_content));
+
+        modeSelection = (TextView) findViewById(R.id.modeSelection);
+        modeSelection.setText(getText(R.string.help_mode_selection));
+
+        modeContinuous = (TextView) findViewById(R.id.modeContinuous);
+        modeContinuous.setText(getText(R.string.help_mode_continuous));
+
+        modeDotted = (TextView) findViewById(R.id.modeDotted);
+        modeDotted.setText(getText(R.string.help_mode_dotted));
+
+
+        roundPlayerHelp = (TextView) findViewById(R.id.roundPlayerHelp);
+        roundPlayerHelp.setText(getText(R.string.help_round_player));
+
+        trianglePlayerHelp = (TextView) findViewById(R.id.trianglePlayerHelp);
+        trianglePlayerHelp.setText(getText(R.string.help_triangle_player));
+
+        squarePlayerHelp = (TextView) findViewById(R.id.squarePlayerHelp);
+        squarePlayerHelp.setText(getText(R.string.help_square_player));
+    }
+}
